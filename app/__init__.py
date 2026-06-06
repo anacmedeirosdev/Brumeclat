43427575ffd9aa96b824278de59d98cb686edee8
anacmedeirosdev <anacmedeiros.dev@gmail.com>
2026-06-06 19:15:39 +0000

    Modificando as configurações do banco

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     from app.routes.auth import auth_bp
